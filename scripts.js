01efc6483f67899655585d5a7e279e21ae8bbb34
Nik Bornemeier <NikolausRB@gmail.com>
2017-12-13 08:17:09 -0700

    fix desktop view

--- scripts.js
+++ scripts.js
@@ -1,45 +1,11 @@
 
 $('.main-content-container').on('click', '.accordion', function () {
-  var acc = document.getElementsByClassName("accordion");
-  var i;
-
-  for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function () {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      }
-    }
-  }
-})
+  $(this).toggleClass("active");
+  $(this).siblings('.accordion').removeClass('active');
+  var panel = this.nextElementSibling;
+  $(panel).toggleClass('display-none');
+  $(panel).siblings('.panel').addClass('display-none');
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// $('.accord-tabs-container').on('click', '.accord-tab', function () {
-//   if (!$(this).attr('class').includes('selected')) {
-//     $(this).siblings().removeClass('accord-tab-selected')
-//     let idNumber = this.id.replace('acTab', '')
-//     let main = $(`#acMain${idNumber}`)
-//     main.toggleClass(`display-none`)
-//     main.siblings().addClass('display-none')
-
-//     // .attr('id').includes(`acMain${idNumber}`)
-//     console.log(main)
-//     $(this).toggleClass('accord-tab-selected')
-//   } 
-// })
 
